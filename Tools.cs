1dee33e61ba8781a1d6e309c9c513ddc3ac0e991
James Dean <james@terradomain.us>
2019-08-18 04:23:07 -0500

    Improves exception reports.
    Now can also display custom exception information through `Exception#Data`.

--- Tools.cs
+++ Tools.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Linq;
+using System.Text;
 
 namespace HawkSoft.BetterAbilityBar {
 
@@ -7,11 +9,30 @@ namespace HawkSoft.BetterAbilityBar {
     /// <summary>
     /// Describes an exception in a log-friendly way.
     /// </summary>
-    public static string ToLogMessage(this Exception exception) => String.Concat(
-      "Exception report...",
-      "\r\n\r\n",
-      exception.ToString()
-    );
+    /// <param name="exception">The exception to build a message for.</param>
+    /// <returns>A log-friendly exception message.</returns>
+    public static string ToLogMessage(this Exception exception) {
+      var sb = new StringBuilder();
+      sb.Append("Exception report...");
+      sb.AppendLine().AppendLine();
+      sb.Append(exception.ToString());
+
+      if (exception.Data.Count > 0) {
+        // List out the custom data stored in the exception.
+        sb.AppendLine().AppendLine();
+        sb.Append("Custom exception data:");
+
+        var de = exception.Data.GetEnumerator();
+        while (de.MoveNext()) {
+          // Indent the data, if it has new-lines.
+          var value = de.Value.ToString().Replace("\n", "\n    ");
+          var data = $"  {de.Key.ToString()} => {value}";
+          sb.AppendLine().Append(data);
+        }
+      }
+
+      return sb.ToString();
+    }
 
   }
 
